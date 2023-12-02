ec1373217e8ed83c0b38d8baa11c32250630e115
Yordan Grozdev <grozdev.yg@gmail.com>
2023-12-02 15:30:13 +0200

    Add project files.

---
+++ TestApp.UnitTests/SongTests.cs
@@ -0,0 +1,73 @@
+using NUnit.Framework;
+
+using System;
+using System.Linq.Expressions;
+
+namespace TestApp.UnitTests;
+
+public class SongTests
+{
+    private Song _song;
+
+    [SetUp]
+    public void SetUp()
+    {
+        this._song = new();
+    }
+
+    [Test]
+    public void Test_AddAndListSongs_ReturnsAllSongs_WhenWantedListIsAll()
+    {
+        // Arrange
+        Song song = new Song();
+        string[] songs = { "Pop_Song1_3:30", "Rock_Song2_4:15", "Pop_Song3_3:00" };
+        string expected = $"Song1{Environment.NewLine}Song2{Environment.NewLine}Song3";
+
+        // Act
+        string result = song.AddAndListSongs(songs, "all");
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_AddAndListSongs_ReturnsFilteredSongs_WhenWantedListIsSpecific()
+    {
+        // Arrange
+        string[] songs = { "Pop_Song1_3:30", "Rock_Song2_4:15", "Pop_Song3_3:00" };
+        string expected = $"Song2";
+
+        // Act
+        string result = this._song.AddAndListSongs(songs, "Rock");
+
+        //Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Test_AddAndListSongs_ReturnsEmptyString_WhenNoSongsMatchWantedList()
+    {
+        // Arrange
+        string[] songs = { "Pop_Song1_3:30", "Rock_Song2_4:15", "Pop_Song3_3:00" };
+        string expected = string.Empty;
+
+        // Act
+        string result = this._song.AddAndListSongs(songs, "Metal");
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Test_AddAndListSongs_ReturnsEmptyString_WhenNoSongsAreGiven()
+    {
+        // Arrange
+        string[] songs = { };
+
+        // Act
+        string result = this._song.AddAndListSongs(songs, "Punk");
+
+        // Assert
+        Assert.AreEqual(string.Empty, result);
+    }
+}
---
+++ TestApp.UnitTests/VehicleTests.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using System;
+
+using TestApp.Vehicle;
+
+namespace TestApp.UnitTests;
+
+public class VehicleTests
+{
+    [Test]
+    public void Test_AddAndGetCatalogue_ReturnsSortedCatalogue()
+    {
+        // Arrange
+        Vehicles vehicles = new Vehicles();
+
+        string[] input =
+        {
+            "Car/Ford/Focus/120",
+            "Car/Toyota/Camry/150",
+            "Truck/Volvo/VNL/500"
+        };
+
+        string expected = $"Cars:{Environment.NewLine}Ford: Focus - 120hp{Environment.NewLine}Toyota: Camry - 150hp{Environment.NewLine}Trucks:{Environment.NewLine}Volvo: VNL - 500kg";
+
+        // Act
+        string result = vehicles.AddAndGetCatalogue(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_AddAndGetCatalogue_RetrunSortedCatalogue_WhenOnlyCarsAreGiven()
+    {
+        // Arrange
+        Vehicles vehicles = new Vehicles();
+        string[] input =
+{
+            "Car/Ford/Focus/120",
+            "Car/Toyota/Camry/150",
+        };
+
+        string expected = $"Cars:{Environment.NewLine}Ford: Focus - 120hp{Environment.NewLine}Toyota: Camry - 150hp{Environment.NewLine}Trucks:";
+        // Act
+        string actual = vehicles.AddAndGetCatalogue(input);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void Test_AddAndGetCatalogue_ReturnsSortedCatalogue_WhenOnlyTrucksAreGiven()
+    {
+        // Arrange
+        Vehicles vehicles = new Vehicles();
+        string[] input = {
+            "Truck/Volvo/VNL/500"
+        };
+
+        string expected = $"Cars:{Environment.NewLine}Trucks:{Environment.NewLine}Volvo: VNL - 500kg";
+
+        // Act
+        string actual = vehicles.AddAndGetCatalogue(input);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void Test_AddAndGetCatalogue_ReturnsEmptyCatalogue_WhenNoDataGiven()
+    {
+        // Arrange
+        Vehicles vehicles = new Vehicles();
+        string[] input = { };
+        string expected = $"Cars:{Environment.NewLine}Trucks:";
+
+        // Act
+        string result = vehicles.AddAndGetCatalogue(input);
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+}
